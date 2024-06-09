0455c9d8c2a27760d7d489249748d269d928e3d6
Manny <mcgrailmanny@gmail.com>
2024-06-09 14:34:40 -0400

    revert lab

--- pittapi/lab.py
+++ pittapi/lab.py
@@ -81,5 +81,12 @@ def get_status():
         if key["name"] in labs:
             total = key["total"]
             in_use = key["active"]
-            statuses.append({"location": key["name"], "isOpen": labs[key["name"]], "total": total, "in_use": in_use})
+            statuses.append(
+                {
+                    "location": key["name"],
+                    "isOpen": labs[key["name"]],
+                    "total": total,
+                    "in_use": in_use}
+            )
+
     return statuses
