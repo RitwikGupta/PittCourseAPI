eb0c45695e6995c7681e4774709ecadbeacee5af
Tianyi Zheng <tianyizheng02@gmail.com>
2024-06-12 22:09:45 -0700

    Update type hints

--- pittapi/library.py
+++ pittapi/library.py
@@ -17,9 +17,11 @@ with this program; if not, write to the Free Software Foundation, Inc.,
 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 """
 
+from __future__ import annotations
+
 import requests
 from html.parser import HTMLParser
-from typing import Any, Dict, List, Union
+from typing import Any
 
 LIBRARY_URL = (
     "https://pitt.primo.exlibrisgroup.com/primaws/rest/pub/pnxs"
@@ -132,7 +134,7 @@ def _extract_facets(facet_fields: list[dict[str, Any]]) -> dict[str, list[dict[s
     return facets
 
 
-def hillman_total_reserved() -> Dict[str, int]:
+def hillman_total_reserved() -> dict[str, int]:
     """Returns a simple count dictionary of the total amount of reserved rooms appointments"""
     count = {}
     resp = requests.get(STUDY_ROOMS_URL)
@@ -145,7 +147,7 @@ def hillman_total_reserved() -> Dict[str, int]:
     return count
 
 
-def reserved_hillman_times() -> List[Dict[str, Union[str, List[str]]]]:
+def reserved_hillman_times() -> list[dict[str, str | list[str]]]:
     """Returns a list of dictionaries of reserved rooms of the Hillman with their respective times"""
     resp = requests.get(STUDY_ROOMS_URL)
     resp = resp.json()
