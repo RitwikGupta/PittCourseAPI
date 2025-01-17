da192a7686506c26e69f512f0f77f101fc8f6676
Tianyi Zheng <tianyizheng02@gmail.com>
2025-01-17 02:54:05 -0800

    Convert gym unit test mock from Python str to HTML
    Convert the mock for gym unit tests from a Python string to an HTML
    file. This makes the mocking of gym unit tests more consistent with how
    unit tests for other modules are mocked. This also avoids unnecessarily
    mocking entire module functions, as only the HTTP requests made by those
    functions need to be mocked. Since the mock is only supposed to contain
    an HTML HTTP response, it makes more sense to keep it as HTML.

--- tests/gym_test.py
+++ tests/gym_test.py
@@ -1,19 +1,22 @@
+from pathlib import Path
 import unittest
 import responses
+
 from pittapi import gym
-from tests.mocks.gym_mocks import mock_gym_html
+
+SAMPLE_PATH = Path() / "tests" / "samples"
 
 
 class GymTest(unittest.TestCase):
     def __init__(self, *args, **kwargs):
         unittest.TestCase.__init__(self, *args, **kwargs)
 
+        with open(SAMPLE_PATH / "gym.html", "r") as f:
+            self.mock_gym_data = f.read()
+
     @responses.activate
-    def test_fetch_gym_info(self):
-
-        responses.add(responses.GET, gym.GYM_URL, body=mock_gym_html, status=200)
-
-        gym_info = gym.get_all_gyms_info()
+    def test_get_all_gyms_info(self):
+        responses.add(responses.GET, gym.GYM_URL, body=self.mock_gym_data, status=200)
         expected_info = [
             gym.Gym(name="Baierl Rec Center", last_updated="07/09/2024 09:05 AM", current_count=100, percent_full=50),
             gym.Gym(
@@ -35,35 +38,41 @@ class GymTest(unittest.TestCase):
             gym.Gym(name="Pitt Sports Dome", last_updated="07/09/2024 09:05 AM", current_count=15, percent_full=20),
         ]
 
+        gym_info = gym.get_all_gyms_info()
+
         self.assertEqual(gym_info, expected_info)
 
     @responses.activate
     def test_get_gym_info(self):
-        responses.add(responses.GET, gym.GYM_URL, body=mock_gym_html, status=200)
-
-        gym_info = gym.get_gym_info("Baierl Rec Center")
+        responses.add(responses.GET, gym.GYM_URL, body=self.mock_gym_data, status=200)
         expected_info = gym.Gym(
             name="Baierl Rec Center", last_updated="07/09/2024 09:05 AM", current_count=100, percent_full=50
         )
+
+        gym_info = gym.get_gym_info("Baierl Rec Center")
+
         self.assertEqual(gym_info, expected_info)
 
     @responses.activate
     def test_invalid_gym_name(self):
-        responses.add(responses.GET, gym.GYM_URL, body=mock_gym_html, status=200)
+        responses.add(responses.GET, gym.GYM_URL, body=self.mock_gym_data, status=200)
 
         gym_info = gym.get_gym_info("Invalid Gym Name")
+
         self.assertIsNone(gym_info)
 
     @responses.activate
     def test_valid_gym_name_not_all_info(self):
-        responses.add(responses.GET, gym.GYM_URL, body=mock_gym_html, status=200)
+        responses.add(responses.GET, gym.GYM_URL, body=self.mock_gym_data, status=200)
 
         gym_info = gym.get_gym_info("Bellefield Hall: Court & Dance Studio")
+
         self.assertIsNone(gym_info)
 
     @responses.activate
     def test_percentage_value_error(self):
-        responses.add(responses.GET, gym.GYM_URL, body=mock_gym_html, status=200)
+        responses.add(responses.GET, gym.GYM_URL, body=self.mock_gym_data, status=200)
 
-        gym_info = gym.get_gym_info("Bellefield Hall: Fitness Center & Weight Roomo")
+        gym_info = gym.get_gym_info("Bellefield Hall: Fitness Center & Weight Room")
+
         self.assertIsNone(gym_info)
